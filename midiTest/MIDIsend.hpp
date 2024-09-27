33558d1bf0bb2023a414c52c34c5d47196552945
Pikku Hurja <pikku.hurja@gmail.com>
2024-09-27 14:28:27 +0300

    quick commit -_-

--- midiTest/MIDIsend.hpp
+++ midiTest/MIDIsend.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "MIDIcommandByte.hpp"
+#include "MIDIenums.hpp"
 
+using fp_2_14 = unsigned short;
 namespace MIDI {
     inline unsigned short getShort(unsigned char lsb, unsigned char msb){
         unsigned short s = msb;
@@ -51,10 +53,29 @@ namespace MIDI {
         midi.write(capData(msb));     //ensures that it is max 127
     }
 
+
+
     template<typename T>
-    inline bool sendPitch(T& midi, MCU::PitchBendMapping pitch_channel, unsigned short lsb_msb_14bit){
+    inline bool sendPitch(T& midi, MCU::PitchBendMapping pitch_channel, fp_2_14 lsb_msb_14bit){
         midi.write(CommandByte::make(Command::PitchBendChange, pitch_channel).mCommandByte);
         midi.write(lsb_msb_14bit & 0b01111111);     //ensures that it is max 127
         midi.write(capData(lsb_msb_14bit >> 7));             //ensures that it is max 127
+    }
+
+    // represents 4 decimal places
+
+    constexpr fp_2_14 max = 16384;
+    constexpr size_t decimalPlaces = 4;
+    char loadedNumber[7] = {'0','.','0','0','0','0', 0};
+    const char* toString(fp_2_14 fp){
+        loadedNumber[0] = '0'+(fp>>14);
+            //trust me bro
+        int fract = ((fp&0b0011111111111111) * 10000)/max;
+        for(size_t i = 0; i < decimalPlaces; i++){
+            loadedNumber[sizeof(loadedNumber)-2 - i] = '0' + fract%10;
+            fract/=10;
+        }
+        loadedNumber[sizeof(loadedNumber)-1] = 0;
+        return loadedNumber;
     }
 }
