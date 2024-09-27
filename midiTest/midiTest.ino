c391947238d656dcbf97ba92e3cc0d9464a2b64a
LukioTools <114951381+LukioTools@users.noreply.github.com>
2024-09-27 16:43:21 +0300

    .

--- midiTest/midiTest.ino
+++ midiTest/midiTest.ino
@@ -17,7 +17,7 @@ MIDI::DeviceControl::NoteRotaryEncoder<USBMIDI_> rotarty1(USBMIDI, MIDI::MCU::Pi
 MIDI::SoftwareControl::NoteBtn<USBMIDI_> playBtnSoftware(USBMIDI, 0, MIDI::MCU::PLAY, 127, 'w');
 MIDI::SoftwareControl::NoteBtn<USBMIDI_> stopBtnSoftware(USBMIDI, 0, MIDI::MCU::STOP, 127, 's');
 
-MIDI::Exclusive exclusive;
+MIDI::Basic basic;
 
 void setup() {
     //pinMode(LED_BUILTIN, OUTPUT);
@@ -31,40 +31,6 @@ void setup() {
 }
 unsigned char i = 0;
 void loop() {
-<<<<<<< HEAD
-	USBMIDI.poll();
-
-    MIDI::Basic basic;
-    basic.mCommandByte = USBMIDI.read();
-    int c = basic.getDataByteCount();
-    
-    if(c != -1) { 
-        for (int i = 0; i < c; i++) {
-            USBMIDI.poll();
-            basic.mData[i] = USBMIDI.read();
-        }
-    } else {
-        MIDI::Exclusive exclusive;
-        int counter = 0;
-        while (true) {
-            counter++;
-        }
-    }
-
-    if(basic.getCommand() != MIDI::ChannelAftertouch){
-        Serial.print(basic.getCommandName());
-        Serial.print(" ");
-        Serial.println(basic.getChannel());
-    }
-
-	USBMIDI.flush();
-=======
-    //Serial.println(MIDI::maxSize);
-    //Serial.println(MIDI::MCU::toString(static_cast<MIDI::MCU::NoteMapping>(i++)));
-    //if(119 < i)
-    //  i = 0;
-    //delay(100);
-    
     if(USBMIDI.available()){
         if(!basic.read(USBMIDI)){
             rotarty1.updateValue(basic);
@@ -85,5 +51,4 @@ void loop() {
 
     delay(1);
     
->>>>>>> b16341e5f40f0e47262fba7e9130b2858f1fbea1
 }
