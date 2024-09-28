fa5e0ddcd85b58edcd16ad66f0f072ad19263228
LukioTools <114951381+LukioTools@users.noreply.github.com>
2024-09-28 13:29:52 +0300

    ..

--- midiTest/MIDIUSB.hpp
+++ midiTest/MIDIUSB.hpp
@@ -1,4 +1,19 @@
+#include <string.h>
 #pragma once
+
+// -------------------------
+// error checking 
+#if ARDUINO < 10606
+#error MIDIUSB requires Arduino IDE 1.6.6 or greater. Please update your IDE.
+#endif
+
+#if !defined(USBCON)
+#error MIDIUSB can only be used with an USB MCU.
+#endif
+
+#include "USBCore.h"
+#include <avr/interrupt.h>
+
 
 #include "USBAPI.h"
 #include "PluggableUSB.h"
@@ -9,36 +24,270 @@
 #include <cstdint>
 #endif
 
+#define MIDI_AC_INTERFACE 	pluggedInterface	// MIDI AC Interface
+#define MIDI_INTERFACE 		((uint8_t)(pluggedInterface+1))
+#define MIDI_FIRST_ENDPOINT pluggedEndpoint
 #define MIDI_ENDPOINT_OUT	pluggedEndpoint
 #define MIDI_ENDPOINT_IN	((uint8_t)(pluggedEndpoint+1))
+
+
 
 #define MIDI_RX MIDI_ENDPOINT_OUT
 #define MIDI_TX MIDI_ENDPOINT_IN
 
+#define is_write_enabled(x)			(1)
+
 #define EP_TYPE_BULK_IN_MIDI 		EP_TYPE_BULK_IN
 #define EP_TYPE_BULK_OUT_MIDI 		EP_TYPE_BULK_OUT
 
+
+
+#define MIDI_AUDIO								0x01    //(USB interface: Audio)    https://developerhelp.microchip.com/xwiki/bin/view/applications/usb/how-it-works/device-classes/
+#define MIDI_AUDIO_CONTROL						0x01 	//(USB interface: Audio)    https://developerhelp.microchip.com/xwiki/bin/view/applications/usb/how-it-works/device-classes/
+
+#define MIDI_CS_INTERFACE						0x24 	// https://warrantyvoids.github.io/ModSynth/modules/midi2cv/midi-descriptor
+#define MIDI_CS_ENDPOINT						0x25 	// https://warrantyvoids.github.io/ModSynth/modules/midi2cv/midi-descriptor
+
+#define MIDI_STREAMING							0x3 	// https://www.usb.org/sites/default/files/USB%20MIDI%20v2_0.pdf
+#define MIDI_JACK_EMD							0x01  	// https://www.usb.org/sites/default/files/USB%20MIDI%20v2_0.pdf
+#define MIDI_JACK_EXT							0X02 	// https://www.usb.org/sites/default/files/USB%20MIDI%20v2_0.pdf
+
+
+#define D_AC_INTERFACE(_streamingInterfaces, _MIDIInterface) \
+	{9, MIDI_CS_INTERFACE, 0x1, 0x0100, 0x0009, _streamingInterfaces, (uint8_t)(_MIDIInterface) }
+
+#define D_AS_INTERFACE \
+	{ 0x7, MIDI_CS_INTERFACE, 0x01,0x0100, 0x0041}
+
+#define D_MIDI_INJACK(jackProp, _jackID) \
+	{ 0x06, MIDI_CS_INTERFACE, 0x02, jackProp, _jackID, 0  }
+
+#define D_MIDI_OUTJACK(jackProp, _jackID, _nPins, _srcID, _srcPin) \
+	{ 0x09, MIDI_CS_INTERFACE, 0x3, jackProp, _jackID, _nPins, _srcID, _srcPin, 0  }
+
+#define D_MIDI_JACK_EP(_addr,_attr,_packetSize) \
+	{ 9, 5, _addr,_attr,_packetSize, 0, 0, 0}
+
+#define D_MIDI_AC_JACK_EP(_nMIDI, _iDMIDI) \
+	{ 5, MIDI_CS_ENDPOINT, 0x1, _nMIDI, _iDMIDI}
+
+#define D_CDCCS(_subtype,_d0,_d1)	{ 5, 0x24, _subtype, _d0, _d1 }
+#define D_CDCCS4(_subtype,_d0)		{ 4, 0x24, _subtype, _d0 }
+
+
 using epTypeT = uint8_t;
 
-struct midiEventPacket_t
-{
+struct midiEventPacket_t {
 	uint8_t header;
 	uint8_t byte1;
 	uint8_t byte2;
 	uint8_t byte3;
 };
 
-class MIDI_USB : public PluggableUSBModule{
-    epTypeT epType[2] = {EP_TYPE_BULK_OUT_MIDI, EP_TYPE_BULK_IN_MIDI};
-    MIDI_USB() : PluggableUSBModule(2,2, epType){
-        PluggableUSB().plug(this);
-    }
+
+
+_Pragma("pack(1)")
+struct MIDI_ACInterfaceDescriptor {
+	uint8_t len;		// 9
+	uint8_t dtype;		// 4
+	uint8_t dsubType;
+	uint16_t bcdADc;
+	uint16_t wTotalLength;
+	uint8_t bInCollection;
+	uint8_t interfaceNumbers;
+};
+
+struct MIDI_ASInterfaceDescriptor {
+	uint8_t len;		// 9
+	uint8_t dtype;		// 4
+	uint8_t dsubType;
+	uint16_t bcdADc;
+	uint16_t wTotalLength;
+};
+
+struct MIDIJackinDescriptor {
+	uint8_t len;		// 9
+	uint8_t dtype;		// 4
+	uint8_t dsubType;
+	uint8_t jackType;
+	uint8_t jackID;
+	uint8_t jackStrIndex;
+};
+
+struct MIDIJackOutDescriptor {
+	uint8_t len;		// 9
+	uint8_t dtype;		// 4
+	uint8_t dsubType;
+	uint8_t jackType;
+	uint8_t jackID;
+	uint8_t nPins;
+	uint8_t srcJackID;
+	uint8_t srcPinID;
+	uint8_t jackStrIndex;
+};
+
+struct MIDI_EPDescriptor {
+	EndpointDescriptor len;		// 9
+	uint8_t refresh;		// 4
+	uint8_t sync;
+};
+
+struct MIDI_EP_ACDescriptor {
+	uint8_t len;		// 5
+	uint8_t dtype;		// 0x24
+	uint8_t subtype;
+	uint8_t embJacks;
+	uint8_t jackID;
+};
+
+struct MIDIDescriptor {
+	//	IAD
+	IADDescriptor                      iad;
+	// MIDI Audio Control Interface
+	InterfaceDescriptor                Audio_ControlInterface;
+	MIDI_ACInterfaceDescriptor         Audio_ControlInterface_SPC;
+
+	// MIDI Audio Streaming Interface
+	InterfaceDescriptor                Audio_StreamInterface;
+	MIDI_ASInterfaceDescriptor         Audio_StreamInterface_SPC;
+
+	MIDIJackinDescriptor               MIDI_In_Jack_Emb;
+	MIDIJackinDescriptor               MIDI_In_Jack_Ext;
+	MIDIJackOutDescriptor              MIDI_Out_Jack_Emb;
+	MIDIJackOutDescriptor              MIDI_Out_Jack_Ext;
+
+	MIDI_EPDescriptor                  MIDI_In_Jack_Endpoint;
+	MIDI_EP_ACDescriptor               MIDI_In_Jack_Endpoint_SPC;
+	MIDI_EPDescriptor                  MIDI_Out_Jack_Endpoint;
+	MIDI_EP_ACDescriptor               MIDI_Out_Jack_Endpoint_SPC;
 };
 
 
-//MIDI_::MIDI_(void) : PluggableUSBModule(2, 2, epType)
-//{
-//	epType[0] = EP_TYPE_BULK_OUT_MIDI;	// MIDI_ENDPOINT_OUT
-//	epType[1] = EP_TYPE_BULK_IN_MIDI;		// MIDI_ENDPOINT_IN
-//	PluggableUSB().plug(this);
-//}
+struct USBBuffer{
+    uint8_t event[USB_EP_SIZE];
+
+    uint8_t mBegin = 0;
+    uint8_t mEnd = 0;
+
+    uint8_t size(){
+        return mEnd - mBegin;
+    }
+
+    uint8_t* begin(){
+        return event+mBegin;
+    }
+
+    uint8_t* end(){
+        return event+mEnd;
+    }
+
+    void increase(uint8_t s) { mEnd+=s; }
+    
+    template<typename T>
+    T* read(){
+        if(sizeof(T) < size()) return nullptr;
+        T* t = reinterpret_cast<T*>(begin());
+        mBegin+=sizeof(T);
+        return t;
+    }
+
+    void finalize(){
+        uint8_t s = size();
+        for (int i = 0; i < s; i++) {
+            event[i] = event[mBegin+i];
+        }
+        mBegin = 0;
+        mEnd = s;
+    }
+
+    
+    int readUSB(uint8_t usb_endpoint){
+        if(!USB_Available(usb_endpoint)) {
+            return 0;
+        }
+        int c = USB_Recv(usb_endpoint, end(), USB_EP_SIZE-mEnd);
+        increase(c);
+        return c;
+    }
+
+};
+
+USBBuffer midi_rx_buffer;
+
+
+class MIDI_USB : public PluggableUSBModule{
+    epTypeT epType[2] = {EP_TYPE_BULK_OUT_MIDI, EP_TYPE_BULK_IN_MIDI};
+
+
+protected:
+    int getInterface(uint8_t* interfaceNum) override {
+        interfaceNum[0] += 2;	// uses 2 interfaces
+        MIDIDescriptor _midiInterface =
+        {
+            D_IAD(MIDI_AC_INTERFACE, 2, MIDI_AUDIO, MIDI_AUDIO_CONTROL, 0),
+            D_INTERFACE(MIDI_AC_INTERFACE,0,MIDI_AUDIO,MIDI_AUDIO_CONTROL,0),
+            D_AC_INTERFACE(0x1, MIDI_INTERFACE),
+            D_INTERFACE(MIDI_INTERFACE,2, MIDI_AUDIO,MIDI_STREAMING,0),
+            D_AS_INTERFACE,
+            D_MIDI_INJACK(MIDI_JACK_EMD, 0x1),
+            D_MIDI_INJACK(MIDI_JACK_EXT, 0x2),
+            D_MIDI_OUTJACK(MIDI_JACK_EMD, 0x3, 1, 2, 1),
+            D_MIDI_OUTJACK(MIDI_JACK_EXT, 0x4, 1, 1, 1),
+            D_MIDI_JACK_EP(USB_ENDPOINT_OUT(MIDI_ENDPOINT_OUT),USB_ENDPOINT_TYPE_BULK,USB_EP_SIZE),
+            D_MIDI_AC_JACK_EP(1, 1),
+            D_MIDI_JACK_EP(USB_ENDPOINT_IN(MIDI_ENDPOINT_IN),USB_ENDPOINT_TYPE_BULK,USB_EP_SIZE),
+            D_MIDI_AC_JACK_EP (1, 3)
+        };
+        return USB_SendControl(0, &_midiInterface, sizeof(_midiInterface));
+    }
+
+    int getDescriptor(USBSetup& setup __attribute__((unused)) ) override {return 0;}
+
+    bool setup(USBSetup& setup __attribute__((unused)) ) override {return false;}
+
+
+    uint8_t getShortName(char* name) override {
+        memcpy(name, "MIDI", 4);
+        return 4;
+    }
+    
+
+public:
+    MIDI_USB() : PluggableUSBModule(2,2, epType){
+        epType[0] = EP_TYPE_BULK_OUT_MIDI;
+        epType[1] = EP_TYPE_BULK_IN_MIDI;
+    }
+
+    void begin(){
+        PluggableUSB().plug(this);
+    }
+
+    void poll(){
+        midi_rx_buffer.readUSB(MIDI_RX);
+    }
+
+    void finalize(){
+        midi_rx_buffer.finalize();
+    }
+
+    template<typename T>
+    T* read() {
+        return midi_rx_buffer.read<T>();
+    }
+
+    void flush(){
+        USB_Flush(MIDI_TX);
+    }
+
+    size_t write(const uint8_t *buffer, size_t size){
+        if(!is_write_enabled(MIDI_TX)) return 0;        // in case no one is listening we are just going to drop packets, USB_Send might freeze in case no one listens.
+
+        int  r = USB_Send(MIDI_TX, buffer, size);       // return amount sent.
+
+        if(r > 0) return r;
+        else return 0;
+    }
+
+};
+
+extern MIDI_USB MidiUSB;
--- midiTest/midiTest.ino
+++ midiTest/midiTest.ino
@@ -3,11 +3,19 @@
 
 #include "MIDIUSB.hpp"
 
+MIDI_USB MidiUSB;
 
 void setup() {
-
+    Serial.begin(9600);
+    MidiUSB.begin();
 }
 
 void loop() {
-    
+    //MidiUSB.poll();
+    Serial.println("data: ");
+    while (byte* rx = MidiUSB.read<byte>()) {
+        Serial.println(*rx);
+    }
+
+    MidiUSB.finalize();
 }
