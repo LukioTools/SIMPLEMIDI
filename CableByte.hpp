68c3f6a8e304357dfb8e2900069465d7a28a152d
PikkuHurja <pikku.hurja@gmail.com>
2024-09-29 21:43:43 +0300

    quick commit -_-

--- CableByte.hpp
+++ CableByte.hpp
@@ -20,27 +20,8 @@
 
 #pragma once
 
+#include "Enums.hpp"
 namespace MIDI {
-
-    enum CodeIndex : unsigned char{
-        MISC,
-        CABLE_EVENT,
-        SYSMSG_2,
-        SYSMSG_3,
-        SYSEX,
-        SYSMSG_SYSEND_1,
-        SYSEND_2,
-        SYSEND_3,
-        NOTE_OFF,
-        NOTE_ON,
-        POLY_KEY,
-        CTRL_CHANGE,
-        PROG_CHANGE,
-        CHAN_PRESS,
-        PITCH_CHANGE,
-        BYTE_1,
-
-    };
 
     struct CableByte{
         unsigned char mCableByte;
