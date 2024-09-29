c05e72617943aaf4c07c12529e70ba60e10adaee
PikkuHurja <pikku.hurja@gmail.com>
2024-09-29 17:59:55 +0300

    quick commit -_-

--- MIDIUSB.hpp
+++ MIDIUSB.hpp
@@ -22,6 +22,9 @@
 #include <cstdint>
 #endif
 
+#include "USBBuffer.hpp"
+
+
 #define MIDI_AC_INTERFACE 	pluggedInterface	// MIDI AC Interface
 #define MIDI_INTERFACE 		((uint8_t)(pluggedInterface+1))
 #define MIDI_FIRST_ENDPOINT pluggedEndpoint
@@ -160,102 +163,6 @@ struct MIDIDescriptor {
 	MIDI_EP_ACDescriptor               MIDI_Out_Jack_Endpoint_SPC;
 };
 
-
-struct midipacket{
-    uint8_t m_event;
-	uint8_t m_data[3];
-};
-
-template <int Tsize>
-struct USBBuffer{
-    uint8_t event[Tsize];
-    // [0,0,0,0,0]
-    //  |
-    //  
-
-    uint8_t mBegin = 0;
-    uint8_t mEnd = 0;
-
-    uint8_t size(){
-        return mEnd - mBegin;
-    }
-
-    uint8_t* begin(){
-        return event+mBegin;
-    }
-
-    uint8_t* end(){
-        return event+mEnd;
-    }
-
-    void increase(uint8_t s) { mEnd+=s; }
-    
-    template<typename T>
-    T* read(){
-        auto p = peek<T>();
-        if(p)
-            mBegin+=sizeof(T);
-        return p;
-    }
-
-    template<typename T>
-    T* read(uint8_t usb_endpoint){
-        auto p = peek<T>(usb_endpoint);
-        if(p)
-            mBegin+=sizeof(T);
-        return p;
-    }
-
-    template<typename T>
-    T* peek(){
-        if(sizeof(T) > size()) return nullptr;
-        T* t = reinterpret_cast<T*>(begin());
-        return t;
-    }
-
-    template<typename T>
-    T* peek(uint8_t usb_endpoint){
-        if(sizeof(T) > size()){
-            readUSB(usb_endpoint);
-            
-            if(sizeof(T) > size()) 
-                return nullptr;
-        } 
-        T* t = reinterpret_cast<T*>(begin());
-        return t;
-    }
-
-    void finalize(){
-        if(mBegin == 0) return;
-
-        uint8_t s = size();
-        for (int i = 0; i < s; i++) {
-            event[i] = event[mBegin+i];
-        }
-        mBegin = 0;
-        mEnd = s;
-    }
-
-    int available(){
-        return USB_EP_SIZE - mEnd;
-    }
-
-    
-    int readUSB(uint8_t usb_endpoint){
-        finalize();
-        if(!USB_Available(usb_endpoint)) {
-            return 0;
-        }
-
-        midipacket midiEvent;
-        int c = USB_Recv(usb_endpoint, end(), available());
-        if(c > 0)
-            increase(c);
-        
-        return c;
-    }
-
-};
 
 USBBuffer<USB_EP_SIZE> midi_rx_buffer;
 
---
+++ USBBuffer.hpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <stdint.h>
+#if !defined(USBCON)
+//#error MIDIUSB can only be used with an USB MCU.
+#endif
+
+#if !defined(ARDUINO_ARCH_AVR)
+//#error MIDIUSB can only be used with AVR boards.
+#endif
+
+#ifndef ARDUINO
+int USB_Available(int);
+#endif
+
+
+template <int Tsize>
+struct USBBuffer{
+    uint8_t event[Tsize];
+    // [0,0,0,0,0]
+    //  |
+    //  
+
+    uint8_t mBegin = 0;
+    uint8_t mEnd = 0;
+
+    uint8_t size(){
+        return mEnd - mBegin;
+    }
+
+    uint8_t* begin(){
+        return event+mBegin;
+    }
+
+    uint8_t* end(){
+        return event+mEnd;
+    }
+
+    void increase(uint8_t s) { mEnd+=s; }
+    
+    template<typename T>
+    T* read(){
+        auto p = peek<T>();
+        if(p)
+            mBegin+=sizeof(T);
+        return p;
+    }
+
+    template<typename T>
+    T* read(uint8_t usb_endpoint){
+        auto p = peek<T>(usb_endpoint);
+        if(p)
+            mBegin+=sizeof(T);
+        return p;
+    }
+
+    template<typename T>
+    T* peek(){
+        if(sizeof(T) > size()) return nullptr;
+        T* t = reinterpret_cast<T*>(begin());
+        return t;
+    }
+
+    template<typename T>
+    T* peek(uint8_t usb_endpoint){
+        if(sizeof(T) > size()){
+            readUSB(usb_endpoint);
+            
+            if(sizeof(T) > size()) 
+                return nullptr;
+        } 
+        T* t = reinterpret_cast<T*>(begin());
+        return t;
+    }
+
+    void finalize(){
+        if(mBegin == 0) return;
+
+        uint8_t s = size();
+        for (int i = 0; i < s; i++) {
+            event[i] = event[mBegin+i];
+        }
+        mBegin = 0;
+        mEnd = s;
+    }
+
+    int available(){
+        return Tsize - mEnd;
+    }
+
+    
+    int readUSB(uint8_t usb_endpoint){
+        finalize();
+        if(!USB_Available(usb_endpoint)) {
+            return 0;
+        }
+
+        int c = USB_Recv(usb_endpoint, end(), available());
+        if(c > 0)
+            increase(c);
+        
+        return c;
+    }
+
+};
