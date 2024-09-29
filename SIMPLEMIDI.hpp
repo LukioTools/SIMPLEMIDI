fa49116ab9fd682ecd7f5957164fa893093db1fe
LukioTools <114951381+LukioTools@users.noreply.github.com>
2024-09-29 18:02:22 +0300

    .

--- SIMPLEMIDI.hpp
+++ SIMPLEMIDI.hpp
@@ -1,5 +1,14 @@
 #ifndef MIDIUSB_h
 #define MIDIUSB_h
 
+#include "CableByte.hpp"
+#include "CommandByte.hpp"
+#include "DeviceControl.hpp"
+#include "Enums.hpp"
+#include "Event.hpp"
+#include "FP2_16.hpp"
+#include "MIDIUSB.hpp"
+#include "Send.hpp"
+#include "USBBuffer.hpp"
 
 #endif	/* MIDIUSB_h */
